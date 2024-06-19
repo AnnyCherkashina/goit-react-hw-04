e32997e2f679ca19c3ae6a885709f76f9a6035e6
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2024-06-19 17:45:26 +0300

    update hw 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { fetchImages } from "./services/api";
+//components
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import SearchBar from "./components/SearchBar/SearchBar";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+  const [offset, setOffset] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [imgURL, setImgURL] = useState({});
+
+  useEffect(() => {
+    const getImages = async () => {
+      setError(false);
+      setIsLoading(true);
+      try {
+        const imagesList = await fetchImages(query, offset);
+        setImages([...images, ...imagesList.results]);
+        setTotalItems(imagesList.total);
+      } catch (error) {
+        console.log(error.message);
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getImages();
+  }, [query, offset]);
+
+  function handleLoadMore() {
+    setOffset(offset + 1);
+  }
+
+  const handleChangeQuery = (queryStr) => {
+    setQuery(queryStr);
+    setImages([]);
+  };
+
+  const openModal = (imgURL) => {
+    setIsOpen(true);
+    setImgURL(imgURL);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <SearchBar setQuery={handleChangeQuery} />
+      {error && <ErrorMessage />}
+      <ImageGallery images={images} openModal={openModal} />
+      {!isLoading && images.length < totalItems && !error && (
+        <LoadMoreBtn handleLoadMore={handleLoadMore} />
+      )}
+
+      {isLoading && <Loader />}
+      <ImageModal closeModal={closeModal} isOpen={isOpen} imgURL={imgURL} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,32 @@
+import css from "./SearchBar.module.css";
+import { Formik, Form, Field } from "formik";
+
+const SearchBar = ({ setQuery }) => {
+    const handleSubmit = (data, options) => {
+        console.log(data);
+        setQuery(data.query);
+        options.resetForm();
+    };
+    const initialValues = { query: "" };
+    return (
+        <>
+            <header>
+                <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                    <Form className={css.form}>
+                        <Field
+                            className={css.field}
+                            type="text"
+                            name="query"
+                            placeholder="Search..."
+                        />
+                        <button className={css.btn} type="submit">
+                            Search
+                        </button>
+                    </Form>
+                </Formik>
+            </header>
+        </>
+    );
+};
+
+export default SearchBar;
